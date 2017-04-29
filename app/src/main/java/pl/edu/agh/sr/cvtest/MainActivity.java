61685851818f123b97397541b2427284ecabb029
Lukasz Wawrzyk <lukasz.wawrzyk@gmail.com>
2017-04-29 11:46:44 +0200

    Initial

---
+++ app/src/main/java/pl/edu/agh/sr/cvtest/MainActivity.java
@@ -0,0 +1,30 @@
+package pl.edu.agh.sr.cvtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, OpenCVTutorial.class);
+        Log.d("XXXX", "CLICKED");
+        startActivity(intent);
+    }
+
+}
