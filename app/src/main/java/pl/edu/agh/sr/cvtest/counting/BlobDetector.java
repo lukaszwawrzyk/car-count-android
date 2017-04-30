bcb5bf8e2448b314973f1fc66b010b7c8ef6431a
Lukasz Wawrzyk <lukasz.wawrzyk@gmail.com>
2017-04-30 10:41:13 +0200

    Good nice, 4 screens, vid 1 done

--- app/src/main/java/pl/edu/agh/sr/cvtest/counting/BlobDetector.java
+++ app/src/main/java/pl/edu/agh/sr/cvtest/counting/BlobDetector.java
@@ -1,21 +1,21 @@
 package pl.edu.agh.sr.cvtest.counting;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 import org.opencv.core.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.opencv.imgproc.Imgproc.*;
-import static org.opencv.core.Core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Core;
 
 public class BlobDetector {
 
-    private Mat frame1;
-    private Mat frame2;
+    private Mat storedPrevFrame;
+    private Mat storedCurrentFrame;
 
-    private Mat difference;
-    private Mat threshold;
+    private Mat transformedFrame;
 
     private Scalar SCALAR_BLACK = new Scalar(0, 0, 0);
     private Scalar SCALAR_WHITE = new Scalar(255, 255, 255);
@@ -24,65 +24,119 @@ public class BlobDetector {
     private Scalar SCALAR_GREEN = new Scalar(0, 255, 0);
 
     public Mat getMovingObjects(Mat newFrame) {
-        if (frame1 == null && frame2 == null) {
-            frame2 = newFrame.clone();
-            difference = new Mat(newFrame.size(), newFrame.type());
-            threshold = new Mat(newFrame.size(), newFrame.type());
+        if (notInitialized()) {
+            initFromFirstFrame(newFrame);
             return newFrame;
         }
-        if (frame1 != null) frame1.release();
-        frame1 = frame2;
-        frame2 = newFrame.clone();
+        shiftFrames(newFrame);
 
-        Mat frame1Copy = frame1.clone();
-        Mat frame2Copy = frame2.clone();
-        cvtColor(frame1Copy, frame1Copy, COLOR_BGR2GRAY);
-        cvtColor(frame2Copy, frame2Copy, COLOR_BGR2GRAY);
-        GaussianBlur(frame1Copy, frame1Copy, new Size(5, 5), 0);
-        GaussianBlur(frame2Copy, frame2Copy, new Size(5, 5), 0);
-        absdiff(frame1Copy, frame2Copy, difference);
-        threshold(difference, threshold, 30, 255, THRESH_BINARY);
+        FourWayDisplay display = new FourWayDisplay(transformedFrame);
+        Mat prevFrame = storedPrevFrame;
+        Mat currFrame = storedCurrentFrame.clone();
+        bwBlur(prevFrame);
+        bwBlur(currFrame);
+        display.put1(currFrame);
+        diffWithThreshold(prevFrame, currFrame, transformedFrame);
+        dilate(transformedFrame);
+        display.put2(transformedFrame);
+        List<Blob> blobs = getBlobs();
+        drawBlobs(blobs, transformedFrame);
+        display.put3(transformedFrame);
+        drawBlobRects(currFrame, transformedFrame, blobs);
+        display.put4(transformedFrame);
+        return display.getOutputImg();
+    }
 
-        Mat structuringElement5x5 = getStructuringElement(MORPH_RECT, new Size(5, 5));
-        dilate(threshold, threshold, structuringElement5x5);
-        dilate(threshold, threshold, structuringElement5x5);
-        erode(threshold, threshold, structuringElement5x5);
+    private boolean notInitialized() {
+        return storedPrevFrame == null && storedCurrentFrame == null;
+    }
 
-        List<MatOfPoint> contours = new ArrayList<>();
-        findContours(threshold, contours, new Mat(), RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    private void shiftFrames(Mat newFrame) {
+        if (storedPrevFrame != null) storedPrevFrame.release();
+        storedPrevFrame = storedCurrentFrame;
+        storedCurrentFrame = newFrame.clone();
+    }
 
-        List<MatOfPoint> convexHulls = new ArrayList<>(contours.size());
+    private void initFromFirstFrame(Mat newFrame) {
+        storedCurrentFrame = newFrame.clone();
+        transformedFrame = new Mat(newFrame.size(), newFrame.type());
+    }
 
-        for (int i = 0; i < contours.size(); i++) {
-            MatOfInt hull = new MatOfInt();
-            convexHull(contours.get(i), hull);
-            MatOfPoint hullContour = hull2Points(hull, contours.get(i));
-            hull.release();
-            convexHulls.add(hullContour);
+    private void drawBlobRects(Mat currFrame, Mat dest, List<Blob> blobs) {
+        currFrame.copyTo(dest);
+        for (Blob blob : blobs) {
+            Imgproc.rectangle(dest, blob.boundingRect.tl(), blob.boundingRect.br(), SCALAR_BLUE, 2);
+            Imgproc.circle(dest, blob.centerPosition, 3, SCALAR_GREEN, -1);
         }
+    }
 
+    private void drawBlobs(List<Blob> blobs, Mat out) {
+        out.setTo(SCALAR_BLACK);
+        List<MatOfPoint> hullsOfBlobs = new ArrayList<>();
+        for (Blob blob : blobs) {
+            hullsOfBlobs.add(blob.contour);
+        }
+        Imgproc.drawContours(out, hullsOfBlobs, -1, SCALAR_WHITE, -1);
+    }
+
+    private void bwBlur(Mat prevFrame) {
+        toBW(prevFrame);
+        blur(prevFrame);
+    }
+
+    private void dilate(Mat frame) {
+        Mat structuringElement5x5 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
+        Imgproc.dilate(frame, frame, structuringElement5x5);
+    }
+
+    @NonNull
+    private List<Blob> getBlobs() {
+        List<MatOfPoint> convexHulls = getConvexHullsOfContours();
         List<Blob> blobs = new ArrayList<>();
         for (MatOfPoint convexHull : convexHulls) {
             Blob possibleBlob = new Blob(convexHull);
-
-            if (possibleBlob.boundingRect.area() > 100 &&
-                    possibleBlob.aspectRatio >= 0.2 &&
-                    possibleBlob.aspectRatio <= 1.2 &&
-                    possibleBlob.boundingRect.width > 15 &&
-                    possibleBlob.boundingRect.height > 20 &&
-                    possibleBlob.diagonalSize > 30) {
+            if (isValid(possibleBlob)) {
                 blobs.add(possibleBlob);
             }
         }
+        return blobs;
+    }
 
-        Mat frame2CopyAgain = frame2.clone();
+    private boolean isValid(Blob blob) {
+        return blob.boundingRect.area() > 100 &&
+                blob.aspectRatio >= 0.2 &&
+                blob.aspectRatio <= 1.2 &&
+                blob.boundingRect.width > 15 &&
+                blob.boundingRect.height > 20 &&
+                blob.diagonalSize > 30;
+    }
 
-        for (Blob blob : blobs) {
-            rectangle(frame2CopyAgain, blob.boundingRect.tl(), blob.boundingRect.br(), SCALAR_BLUE, 2);
-            circle(frame2CopyAgain, blob.centerPosition, 3, SCALAR_GREEN, -1);
+    @NonNull
+    private List<MatOfPoint> getConvexHullsOfContours() {
+        List<MatOfPoint> contours = new ArrayList<>();
+        Imgproc.findContours(transformedFrame, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        List<MatOfPoint> convexHulls = new ArrayList<>(contours.size());
+        MatOfInt hull = new MatOfInt();
+        for (int i = 0; i < contours.size(); i++) {
+            Imgproc.convexHull(contours.get(i), hull);
+            MatOfPoint hullContour = hull2Points(hull, contours.get(i));
+            convexHulls.add(hullContour);
         }
+        hull.release();
+        return convexHulls;
+    }
 
-        return frame2CopyAgain;
+    private void diffWithThreshold(Mat prevFrame, Mat nextFrame, Mat output) {
+        Core.absdiff(prevFrame, nextFrame, output);
+        Imgproc.threshold(output, output, 30, 255, Imgproc.THRESH_BINARY);
+    }
+
+    private void blur(Mat prevFrame) {
+        Imgproc.GaussianBlur(prevFrame, prevFrame, new Size(5, 5), 0);
+    }
+
+    private void toBW(Mat prevFrame) {
+        Imgproc.cvtColor(prevFrame, prevFrame, Imgproc.COLOR_BGR2GRAY);
     }
 
     private MatOfPoint hull2Points(MatOfInt hull, MatOfPoint contour) {
@@ -96,7 +150,7 @@ public class BlobDetector {
         return point;
     }
 
-    private void dump(String msg, Mat threshold) {
+    private void dumpMat(String msg, Mat threshold) {
         StringBuilder res = new StringBuilder(msg + "\n");
         for (int col = 0; col < threshold.cols(); col++) {
             for (int row = 0; row < threshold.rows(); row++) {
@@ -109,24 +163,5 @@ public class BlobDetector {
         }
         Log.d("AAA", res.toString());
     }
-
-    /*List<Blob> getBlobs(Mat newFrame) {
-        if (frame1 == null && frame2 == null) {
-            frame2 = newFrame;
-            return Collections.emptyList();
-        }
-        frame1 = frame2;
-        frame2 = newFrame;
-
-        List<Blob> blobs = new ArrayList<>();
-        Mat frame1Copy = frame1.clone();
-        Mat frame2Copy = frame2.clone();
-        cvtColor(frame1Copy, frame1Copy, COLOR_BGR2GRAY);
-        cvtColor(frame2Copy, frame2Copy, COLOR_BGR2GRAY);
-        GaussianBlur(frame1Copy, frame1Copy, new Size(5, 5), 0);
-        GaussianBlur(frame2Copy, frame2Copy, new Size(5, 5), 0);
-        absdiff(frame1Copy, frame2Copy, difference);
-        threshold(difference, threshold, 30, 255, THRESH_BINARY);
-    }*/
 
 }
---
+++ app/src/main/java/pl/edu/agh/sr/cvtest/counting/FourWayDisplay.java
@@ -0,0 +1,39 @@
+package pl.edu.agh.sr.cvtest.counting;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+class FourWayDisplay {
+    private Size halfSize;
+    private Mat tmpMat;
+    private Mat outputImg;
+    private Rect tl;
+    private Rect tr;
+    private Rect bl;
+    private Rect br;
+
+    FourWayDisplay(Mat transformedFrame) {
+        outputImg = new Mat(transformedFrame.size(), transformedFrame.type());
+        halfSize = new Size(outputImg.size().width / 2, outputImg.size().height / 2);
+        tmpMat = new Mat(halfSize, transformedFrame.type());
+        tl = new Rect(0, 0, (int) halfSize.width, (int) halfSize.height);
+        tr = new Rect((int) halfSize.width, 0, (int) halfSize.width, (int) halfSize.height);
+        bl = new Rect(0, (int) halfSize.height, (int) halfSize.width, (int) halfSize.height);
+        br = new Rect((int) halfSize.width, (int) halfSize.height, (int) halfSize.width, (int) halfSize.height);
+    }
+
+    public Mat getOutputImg() { return outputImg; }
+
+    void put1(Mat mat) { put(mat, tl); }
+    void put2(Mat mat) { put(mat, tr); }
+    void put3(Mat mat) { put(mat, bl); }
+    void put4(Mat mat) { put(mat, br); }
+
+    private void put(Mat mat, Rect rect) {
+        Imgproc.resize(mat, tmpMat, halfSize);
+        tmpMat.copyTo(outputImg.submat(rect));
+    }
+
+}
